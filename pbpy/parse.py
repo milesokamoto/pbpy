c9c277e300a97c777a3e7e04b471e3799d79953c
Miles <milesokamoto@utexas.edu>
2019-12-28 12:53:13 -0600

    added functions

--- pbpy/parse.py
+++ pbpy/parse.py
@@ -2,12 +2,18 @@ import dict
 import names
 
 def parse(text):
+    [event, code] = get_event(text)
+    primary = get_primary(text, event)
+    return [primary, code]
 
-def get_code(text):
-    event = (dict.codes[key] for key in dict.codes.keys() if key in text)
-    return event
+def get_event(text):
+    return [[key, dict.codes[key]] for key in dict.codes.keys() if key in text][0]
 
-parse('Ellis singled down the lf line.')
-text = 'Ellis singled down the lf line.'
-for item in parse('Ellis singled down the lf line.'):
-    print(item)
+def get_loc(text):
+    return [dict.loc_codes[key] for key in dict.loc_codes.keys() if key in text][0]
+
+def get_primary(text, event):
+    return text.split(' ' + event)[0]
+
+def get_run(text):
+    return [[key, dict.run_codes[key]] for key in dict.run_codes.keys() if key in text][0]
