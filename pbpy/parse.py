d640d24638a0980dfb51197c21c0350f8dff93ca
Miles <milesokamoto@utexas.edu>
2019-12-28 12:05:48 -0600

    updated file

--- pbpy/parse.py
+++ pbpy/parse.py
@@ -1,1 +1,13 @@
-parse
+import dict
+import names
+
+def parse(text):
+
+def get_code(text):
+    event = (dict.codes[key] for key in dict.codes.keys() if key in text)
+    return event
+
+parse('Ellis singled down the lf line.')
+text = 'Ellis singled down the lf line.'
+for item in parse('Ellis singled down the lf line.'):
+    print(item)
