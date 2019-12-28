6370a2c1bffd54e34c66056fcdd124be2db2a243
Miles <milesokamoto@utexas.edu>
2019-12-28 11:33:21 -0600

    update play.py

--- pbpy/play.py
+++ pbpy/play.py
@@ -1,7 +1,14 @@
-import requests
-import lxml.html as lh
 import pandas as pd
 
 class Play:
     def __init__(self, text):
         self.text = text
+        self.type = get_play_type(self, text)
+        self.primary = get_primary(self, text)
+
+
+    def get_play_type(self, text):
+
+    def get_primary(self, text):
+
+    def execute(self):
