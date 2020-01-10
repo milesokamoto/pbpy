ad4f90f5596044a9b514c9a8d3d6004e42c95048
Miles <milesokamoto@utexas.edu>
2020-01-10 12:38:38 -0600

    added functions to get current batter and defense

--- pbpy/lineup.py
+++ pbpy/lineup.py
@@ -1,9 +1,11 @@
 import scrape
 import pandas as pd
 
+
 class Lineups:
     def __init__(self, game_id):
-        [self.a_lineup, self.a_sub, self.h_lineup, self.h_sub] = get_lineups(game_id)
+        [self.a_lineup, self.a_sub, self.h_lineup,
+            self.h_sub] = get_lineups(game_id)
 
     def make_sub(self, sub, game):
         if sub.team == 'a':
@@ -21,12 +23,29 @@ class Lineups:
         elif sub.team == 'h':
             self.h_lineup = lu
 
+    def get_batter(self, game):
+        if game.half % 2 == 0:
+            return game.lineups.a_lineup.iloc[game.a_order].item()
+        else:
+            return game.lineups.h_lineup.iloc[game.h_order].item()
 
     def all_names(self, team):
         if team == 'h':
             return self.h_lineup['name'].to_list() + self.h_sub
         elif team == 'a':
             return self.a_lineup['name'].to_list() + self.a_sub
+
+    def get_defense(self, team):
+        pos_list = ['P', 'C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF']
+        d = []
+        if team == 'h':
+            l = self.h_lineup
+        elif team == 'a':
+            l = self.a_lineup
+        for p in pos_list:
+            d.append(l.loc[p].item())
+            return d
+
 
 def get_lineups(game_id):
     player = False
@@ -42,7 +61,8 @@ def get_lineups(game_id):
     i = 0
     j = 0
     skip = True
-    table = scrape.get_table('https://stats.ncaa.org/game/box_score/' + str(game_id))
+    table = scrape.get_table(
+        'https://stats.ncaa.org/game/box_score/' + str(game_id))
     for element in table:
         j = 0
         for row in element:
@@ -74,22 +94,27 @@ def get_lineups(game_id):
                     pos_list.append(row.text)
     return compile_lineups(away, away_pos, home, home_pos)
 
+
 def get_index(list, type):
     if type == "l":
         return [i for i, s in enumerate(list) if not '\xa0' in s]
     elif type == "s":
         return [i for i, s in enumerate(list) if '\xa0' in s]
 
+
 def list_index(list, index):
     return [list[i] for i in index]
+
 
 def compile_lineups(away, away_pos, home, home_pos):
     a_lu = list_index(away, get_index(away, 'l'))
     a_lu_pos = list_index(away_pos, get_index(away, 'l'))
-    a_sub = [s.replace('\xa0', '') for s in list_index(away, get_index(away, 's'))]
+    a_sub = [s.replace('\xa0', '')
+             for s in list_index(away, get_index(away, 's'))]
     h_lu = list_index(home, get_index(home, 'l'))
     h_lu_pos = list_index(home_pos, get_index(home, 'l'))
-    h_sub = [s.replace('\xa0', '') for s in list_index(home, get_index(home, 's'))]
-    a_lineup = pd.DataFrame(a_lu, a_lu_pos, columns = ["name"])
-    h_lineup = pd.DataFrame(h_lu, h_lu_pos, columns = ["name"])
+    h_sub = [s.replace('\xa0', '')
+             for s in list_index(home, get_index(home, 's'))]
+    a_lineup = pd.DataFrame(a_lu, a_lu_pos, columns=["name"])
+    h_lineup = pd.DataFrame(h_lu, h_lu_pos, columns=["name"])
     return [a_lineup, a_sub, h_lineup, h_sub]
