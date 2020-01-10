7a2c976c03cbe3ec087a76d1bd11792fe5ca4843
Miles <milesokamoto@utexas.edu>
2020-01-10 12:39:55 -0600

    created run and bat event classes

--- pbpy/play.py
+++ pbpy/play.py
@@ -4,52 +4,92 @@ import names
 
 class Play:
     def __init__(self, text, game):
-        self.play = text
-        self.primary = get_primary(text)
-        self.type = get_play_type()
+        self.text = text
+        self.game = game
+        if self.game.half % 2 == 0:
+            self.off_team = 'a'
+        else:
+            self.off_team = 'h'
+        self.event = get_event(self.text, '')
+        self.primary = self.game.names.match_name(self.off_team, get_primary(text, self.event))
+        self.parts = self.text.split(':')
+        self.events = []
+        if self.primary == self.game.lineups.get_batter(self.game):
+            self.events.append(BatEvent(self.parts.pop(0)))
+            self.type = 'b'
+        else:
+            self.type = 'r'
+        for p in self.parts:
+            if not p == '':
+                self.events.append(RunEvent(p))
 
-        # self.type = get_play_type(self, text)
-        self.event = self.get_event()
-
-
-        # self.secondaries = get_secondaries(self, text)
+    def match_players(self):
+        for e in events:
+            e.player = self.game.names.match_name(self.off_team, e.player)
 
     def execute(self):
-        pass
+        self.match_players()
 
-def bat_event(self):
-    [event, code] = get_event(self.play)
-    primary = get_primary(self.play, event)
-    return [primary, code]
-
-
-def get_event(players):
-    return [[key, dict.codes[key]] for key in dict.codes.keys() if key in self.play][0]
+        if self.type == 'b':
+            if self.off_team == 'a':
+                self.game.a_order = (self.game.a_order + 1) % 9
+            else:
+                self.game.h_order = (self.game.h_order + 1) % 9
 
 
-def get_loc(play):
-    return [dict.loc_codes[key] for key in dict.loc_codes.keys() if key in text][0]
+class BatEvent:
+    def __init__(self, text):
+        self.text = text
+        [self.event, self.code] = get_event(self.text, 'b')
+        self.player = get_primary(self.text, self.event)
+        self.fielders = get_fielders(text, self.event)
+        self.loc = self.get_loc()
+        self.dest = self.get_bat_dest()
+
+    def get_loc(self):
+        loc = [dict.loc_codes[key] for key in dict.loc_codes.keys() if key in self.text]
+        if len(loc) > 0:
+            return loc[0]
+        else:
+            return self.fielders[0]
+
+    def get_bat_dest(self):
+        return [[key, dict.run_codes[key]] for key in dict.run_codes.keys() if key in self.text][-1]
 
 
-def get_primary(play, event):
-    return play.split(' ' + event)[0]
+
+class RunEvent:
+    def __init__(self, text):
+        self.text = text
+        [self.event, self.code] = get_event(self.text, 'r')
+        self.player = get_primary(self.text, self.event)
+        self.start = self.get_run_start()
+        self.end = self.get_run_dest()
+
+    def get_run_start(runner, game):
+        self.start = game.runners.match(runner) + 1
+
+    def get_run_dest(self):
+        return [[key, dict.run_codes[key]] for key in dict.run_codes.keys() if key in self.text][-1]
 
 
-def get_secondaries(text, event):
-    return text.split(':')[1:]
+
+def get_event(text, type):
+    if type == 'b':
+        return [[key, dict.codes[key]] for key in dict.codes.keys() if key in text][0]
+    elif type == 'r':
+        return [[key, dict.run_codes[key]] for key in dict.run_codes.keys() if key in text][0]
+    else:
+        e = [[key, dict.codes[key]] for key in dict.codes.keys() if key in text][0]
+        if e is None:
+            return [[key, dict.run_codes[key]] for key in dict.run_codes.keys() if key in text][0]
+        else:
+            return e
 
 
-def get_run(text):
-    return [[key, dict.run_codes[key]] for key in dict.run_codes.keys() if key in text][0]
+def get_primary(text, event):
+    return text.split(' ' + event[0])[0]
 
 
 def get_fielders(text, event):
     return [dict.pos_codes[key] for key in dict.pos_codes.keys() if key in text.split(' ' + event)[1]]
-
-
-def get_play_type(self, text):
-    pass
-
-
-def get_run_dest(self):
-    pass
