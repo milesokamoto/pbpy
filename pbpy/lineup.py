23b1d07119e0b1a1704e34a1da8a4fa5d1e3a56b
Miles <milesokamoto@utexas.edu>
2020-01-24 10:20:33 -0600

    added substitution functionality

--- pbpy/lineup.py
+++ pbpy/lineup.py
@@ -7,27 +7,40 @@ class Lineups:
         [self.a_lineup, self.a_sub, self.h_lineup,
             self.h_sub] = get_lineups(game_id)
 
-    def make_sub(self, sub, game):
-        if sub.team == 'a':
+    def make_sub(self, s, g):
+        if s.team == 'a':
             lu = self.a_lineup
-        elif sub.team == 'h':
+        elif s.team == 'h':
             lu = self.h_lineup
-        # else:
-        #     raise()
-        if '/' in sub.sub_in:
-            if type(lu.loc['P'].tolist()) != str:
+        if '/' in s.sub_in:
+            if len(lu[lu['pos']=='P']) > 1:
                 lu = lu[0:9]
-        lu.loc[sub.pos.upper(), 'name'] = sub.sub_in
-        if sub.team == 'a':
+        if s.pos == 'pr':
+            for r in g.runners:
+                if r != '':
+                    if r.name == s.sub_out:
+                        r.name = s.sub_in
+        if s.pos == 'ph':
+            order = g.a_order if s.team == 'a' else g.h_order
+            lu.iloc[order]['name'] = s.sub_in
+            lu.iloc[order]['pos'] = 'PH'
+        elif s.sub_out is None:
+            lu.loc[lu['pos'] == s.pos.upper(), 'pos'] = ''
+            lu.loc[lu['name'] == s.sub_in, 'pos'] = s.pos.upper()
+        else:
+            lu.loc[lu['name'] == s.sub_out, 'name'] = s.sub_in
+            lu.loc[lu['name'] == s.sub_in, 'pos'] = s.pos.upper()
+        if s.team == 'a':
             self.a_lineup = lu
-        elif sub.team == 'h':
+        elif s.team == 'h':
             self.h_lineup = lu
+
 
     def get_batter(self, game):
         if game.half % 2 == 0:
-            return game.lineups.a_lineup.iloc[game.a_order].item()
+            return game.lineups.a_lineup.iloc[game.a_order]['name']
         else:
-            return game.lineups.h_lineup.iloc[game.h_order].item()
+            return game.lineups.h_lineup.iloc[game.h_order]['name']
 
     def all_names(self, team):
         if team == 'h':
@@ -43,7 +56,10 @@ class Lineups:
         elif team == 'a':
             l = self.a_lineup
         for p in pos_list:
-            d.append(l.loc[p].item())
+            if len(l[l['pos'] == p]) > 0:
+                d.append(l[l['pos'] == p]['name'].item())
+            else:
+                d.append('')
         return d
 
 
@@ -91,7 +107,7 @@ def get_lineups(game_id):
                     pos_list = []
                     lineup = []
                 elif pos:
-                    pos_list.append(row.text)
+                    pos_list.append(row.text.split('/')[0])
     return compile_lineups(away, away_pos, home, home_pos)
 
 
@@ -115,6 +131,6 @@ def compile_lineups(away, away_pos, home, home_pos):
     h_lu_pos = list_index(home_pos, get_index(home, 'l'))
     h_sub = [s.replace('\xa0', '')
              for s in list_index(home, get_index(home, 's'))]
-    a_lineup = pd.DataFrame(a_lu, a_lu_pos, columns=["name"])
-    h_lineup = pd.DataFrame(h_lu, h_lu_pos, columns=["name"])
+    a_lineup = pd.DataFrame({'name': a_lu, 'pos': a_lu_pos})
+    h_lineup = pd.DataFrame({'name': h_lu, 'pos': h_lu_pos})
     return [a_lineup, a_sub, h_lineup, h_sub]
