1407b1f1e921aac71e3920f65f3709a39826dab2
Miles <milesokamoto@utexas.edu>
2020-02-11 10:03:14 -0600

    added pbp error check helper function

--- pbpy/parse.py
+++ pbpy/parse.py
@@ -4,7 +4,10 @@ import sub
 
 def parse_half(g, half):
     for play in half:
-        parse(play, g)
+        if check_play(play, g):
+            parse(play, g)
+        else:
+            g.play_index += 1
     if g.half < len(g.game)-1:
         g.advance_half()
 
@@ -17,15 +20,15 @@ def parse(pbp_txt, g):
     if type == 's':
         s = sub.Sub(pbp_txt, g)
         g.make_sub(s)
-        if len(g.game[g.half]) > g.play_of_inn + 1:
-            if not get_type(g.game[g.half][g.play_of_inn + 1])[0] == 's':
+        if len(g.game[g.half]) > g.play_index:
+            if not get_type(g.game[g.half][g.play_index])[0] == 's':
                 g.defense = g.get_defense()
     elif type == 'p':
         try:
             p = play.Play(pbp_txt, g)
             g.execute_play(p)
         except:
-            input('ERROR: ' + pbp_txt)
+            # input('ERROR: ' + pbp_txt)
             return None
     g.pbp_no += 1
     g.inn_pbp_no += 1
@@ -33,6 +36,11 @@ def parse(pbp_txt, g):
         return s
     elif type == 'p':
         return p
+
+def check_play(pbp_text, g):
+    if ' pinch hit' in pbp_text and not 'for' in pbp_text:
+        return False
+    return True
 
 def get_type(s): #  PLAY OR SUB -> BREAK DOWN INTO PARTS
     """
