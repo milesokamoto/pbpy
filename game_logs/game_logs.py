b86176d64d265cf2504db5823869d345cfe1a217
Miles <milesokamoto@utexas.edu>
2020-03-03 11:54:20 -0600

    proof of concept for scraping game logs from stats.ncaa.org

---
+++ game_logs/game_logs.py
@@ -0,0 +1,40 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+
+url = "https://stats.ncaa.org/team/29/stats/15204"
+base = "https://stats.ncaa.org/"
+pitching = "&year_stat_category_id=14761"
+fielding = "&year_stat_category_id=14762"
+
+payload = {}
+headers= {}
+
+response = requests.request("GET", url)
+
+doc = lh.fromstring(response.content)
+links = doc.xpath("//table[@id='stat_grid']/tbody/tr/td/a/@href")
+names = doc.xpath("//table[@id='stat_grid']/tbody/tr/td/a/text()")
+link = '/player/index?game_sport_year_ctl_id=15204&org_id=29&stats_player_seq=2308336'
+for i in range(0, len(links)):
+    player_url = base + links[i]
+    name = names[i]
+    player_response = requests.request("GET", player_url)
+    log = lh.fromstring(player_response.content)
+    stats = log.xpath("//div[@id='game_breakdown_div']/table/tr/td/table")
+    headers = []
+    for td in stats[0][1]:
+        headers.append(td.text)
+    rows = []
+    for tr in stats[0][2:]:
+        row = []
+        for td in tr:
+            if len(td) > 0:
+                row.append(td[0].text.replace('\n', '').replace('  ', ''))
+            else:
+                row.append(td.text.replace('\n', '').replace('  ', ''))
+        rows.append(row)
+    game_log = pd.DataFrame(data = rows, columns = headers)
+    game_log.to_csv(name.replace(', ', '_') + '.csv')
+
+response.text
