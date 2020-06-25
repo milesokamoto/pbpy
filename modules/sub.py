6554def306a3dffaa29a0c77d5628570eded4950
Miles <milesokamoto@utexas.edu>
2020-06-25 11:22:44 -0500

    created subclasses of Sub

--- modules/sub.py
+++ modules/sub.py
@@ -2,33 +2,50 @@ import re
 import modules.names as names
 
 class Sub:
-    def __init__(self, text, game):
-        self.team = get_sub_team(game.half, get_sub_type(text))
-        [self.sub_in_short, self.pos, self.sub_out_short] = parse_sub(text)
-        self.match_sub_names(game.names)
+    def __init__(self, team, player):
+        self.team = team
+        self.player = player
 
-    def match_sub_names(self, names):
-        n = names.h_names if self.team == 'h' else names.a_names
-        if '/' in self.sub_in_short:
-            self.sub_in = '/'
-        else:
-            self.sub_in = rev_dict(self.sub_in_short, n)
-        if not self.sub_out_short is None:
-            self.sub_out = rev_dict(self.sub_out_short, n)
-        else:
-            self.sub_out = None
-        if self.sub_in == -1 or self.sub_out == -1:
-            self.team = 'a' if self.team == 'h' else 'h'
-            n = names.h_names if self.team == 'h' else names.a_names
-            self.sub_in = rev_dict(self.sub_in_short, n)
-            if not self.sub_out_short is None:
-                self.sub_out = rev_dict(self.sub_out_short, n)
+    # def match_sub_names(self, names):
+    #     n = names.h_names if self.team == 'h' else names.a_names
+    #     if '/' in self.sub_in_short:
+    #         self.sub_in = '/'
+    #     else:
+    #         self.sub_in = rev_dict(self.sub_in_short, n)
+    #     if not self.sub_out_short is None:
+    #         self.sub_out = rev_dict(self.sub_out_short, n)
+    #     else:
+    #         self.sub_out = None
+    #     if self.sub_in == -1 or self.sub_out == -1:
+    #         self.team = 'a' if self.team == 'h' else 'h'
+    #         n = names.h_names if self.team == 'h' else names.a_names
+    #         self.sub_in = rev_dict(self.sub_in_short, n)
+    #         if not self.sub_out_short is None:
+    #             self.sub_out = rev_dict(self.sub_out_short, n)
         # match = names.match_name(self.team, self.sub_in, 's')
         # if match[1] != self.team:
         #     self.team = match[1]
         # self.sub_in = names.match_name(self.team, self.sub_in, 's')[0]
         # if not self.sub_out is None:
             # self.sub_out = names.match_name(self.team, self.sub_out, 's')[0]
+
+
+class PositionSwitch(Sub):
+    def __init__(self, team, player, pos):
+        super().__init__(team, player)
+        self.pos = pos
+
+class DefensiveSub(Sub):
+    def __init__(self, team, player, sub, pos):
+        super().__init__(team, player)
+        self.sub = sub
+        self.pos = pos
+
+class OffensiveSub(Sub):
+    def __init__(self, team, player, sub, sub_type):
+        super().__init__(team, player)
+        self.sub = sub
+        self.sub_type = sub_type
 
 def rev_dict(value, dict):
     val = [k for k, v in sorted(dict.items(), key=lambda item: item[1]) if v == value]
@@ -50,7 +67,7 @@ def parse_sub(s):
             else:
                 sub[1] = (re.search(r'(?<=to )[0-9a-z]{1,2}', sub[1]).group())
             return sub
-    raise TypeError('No sub')
+    raise TypeError('Not a substitution')
 
 def get_sub_type(s):
     if 'pinch' in s:
