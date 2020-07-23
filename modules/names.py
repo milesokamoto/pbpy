c991840809dc617ad25480b598e5156ee16bb277
Miles Okamoto <48659623+milesok@users.noreply.github.com>
2020-07-23 11:09:13 -0500

    improve pitcher substitution recognition

--- modules/names.py
+++ modules/names.py
@@ -24,12 +24,13 @@ def match_all(lineup, play_list):
     lu = starters[0:9]
     box_names = {s:'' for s in lu}
     pbp_names = []
+    sub_names = []
 
     # loop through play list to get a list of names used in play by play
     for h in range(0, len(play_list)):
         p_no = 0
-        if (lineup.team == 0) ^ (h % 2 == 1):
-            for p in play_list[h]:
+        for p in play_list[h]:
+            if (lineup.team == 0) ^ (h % 2 == 1):
                 if parse.get_type(p) == 'p':
                     p_no += 1
                     n = p.split(' ' + play.find_events(p)[0])[0]
@@ -41,34 +42,50 @@ def match_all(lineup, play_list):
                         n = ph_check[2]
                         if not n in pbp_names and not n is None:
                             pbp_names.append(n)
+            if parse.get_type(p) == 's':
+                sub_check = sub.parse_sub(p)
+                if not sub_check[0] in sub_names and not sub_check[0] is None:
+                    sub_names.append(sub_check[0])
+                if not sub_check[0] is None and not sub_check[1] in sub_names:
+                    sub_names.append(sub_check[1])
+        
     nm = match_helper(box_names, pbp_names)
 
     # look at all defensive plays to match pitchers
     def_plays = game.all_plays(play_list, (lineup.team + 1) % 2)
-    pitcher_subs = [p for p in def_plays if (' to p.' in p and not 'out to p.' in p and not 'up to p.' in p and not '1b to p.' in p) or ' to p for ' in p or '/ for ' in p]
+    pitcher_subs = [p for p in def_plays if parse.get_type(p) == 's' and ' to p' in p or '/ for ' in p]
     if len(starters) > 9 and len(pitcher_subs) > 0:
         if ' for ' in pitcher_subs[0]:
-            nm[starters[9]] = pitcher_subs[0][0:-1].split(' for ')[1]
+            short = pitcher_subs[0][0:-1].split(' for ')[1]
+            if name_similarity(short, starters[9]) >= .5:
+                nm[starters[9]] = short
+            else:
+                nm[starters[9]] = ''                
         # if a position player moves to pitcher and burns the dh (syntax '/ for <name>), this will match the starter to his name
         elif len(pitcher_subs) > 1 and '/ for ' in pitcher_subs[1]:
             short = pitcher_subs[1][0:-1].split(' for ')[1]
-            if name_similarity(short, starters[9]) > .5:
+            if name_similarity(short, starters[9]) >= .5:
                 nm[starters[9]] = short
         else:
-            nm[starters[9]] = '' # this might be a problem if we get to here
+            nm[starters[9]] = ''
     else:
         unmatched = [s for s in nm.keys() if nm[s] == '']
         if len(unmatched) == 1:
-            short = pitcher_subs[0][0:-1].split(' for ')[1]
-            if name_similarity(short, unmatched[0]) > .5:
+            if ' for ' in pitcher_subs[0]:
+                short = pitcher_subs[0][0:-1].split(' for ')[1]
+            else:
+                short = pitcher_subs[0].split(' to p')[0]
+            if name_similarity(short, unmatched[0]) >= .5:
                 nm[unmatched[0]] = short
     pitchers = [p.split(' to p')[0] for p in pitcher_subs if ' to p' in p]
+
     # matching subs
     subs = lineup.subs
     plays = game.all_plays(play_list, '')
     if not subs is None:
         pitcher_no = 0
         for s in subs:
+            matched = False
             # offensive substitution
             if s.pos in ('ph', 'pr'):
                 if not nm[s.sub] == '':
@@ -76,6 +93,7 @@ def match_all(lineup, play_list):
                 # if pbp text says pinch hit/pinch ran it's easy
                     if len(match) > 0:
                         nm[s.name] = match[0]
+                        matched = True
                 # otherwise it might say "to dh for"
                     else:
                         if (s.name in nm.keys() and nm[s.name] == '') or not s.name in nm.keys():
@@ -88,6 +106,7 @@ def match_all(lineup, play_list):
                                 match = [re.match(r'(.*)(?: to dh for )' + nm[s.sub] + r'\.', p).group(1) for p in plays if nm[s.sub] in p[-len(s.sub)-2:]]
                                 if len(match) > 0:
                                     nm[s.name] = match[0]
+                                    matched = True
                                 else:
                                     nm[s.name] = ''
                             else:
@@ -96,6 +115,8 @@ def match_all(lineup, play_list):
                     if not subs[-1] == s:
                         subs.append(s)
                         s = ''
+                    else:
+                        nm[s.name] = ''
             # pitcher substitution - assumes pitchers are listed in correct order
             elif s.pos == 'p':
                 if pitcher_no < len(pitchers):
@@ -103,6 +124,7 @@ def match_all(lineup, play_list):
                         pitcher_no += 1
                     if name_similarity(pitchers[pitcher_no], s.name) >= .5:
                         nm[s.name] = pitchers[pitcher_no]
+                        matched = True
                         if s.order < 9:
                             pitcher_no = 0
                         else:
@@ -112,23 +134,24 @@ def match_all(lineup, play_list):
                             pitcher_no += 1
                         if name_similarity(pitchers[pitcher_no], s.name) >= .5:
                             nm[s.name] = pitchers[pitcher_no]
+                            matched = True
                             pitcher_no = 0
-                        
-                else:
+                if not matched:
                     off_plays = game.all_plays(play_list, (lineup.team) % 2)
                     pitcher_subs2 = [p for p in off_plays if (' to p.' in p and not 'out to p.' in p and not 'up to p.' in p and not '1b to p.' in p) or ' to p for ' in p or '/ for ' in p]
                     pitchers2 = [p.split(' to p')[0] for p in pitcher_subs2 if ' to p' in p]
                     pitcher_no = 0
                     max_sim = .5
                     max_idx = None
-                    while pitcher_no < len(pitchers2)-1:
-                        pitcher_no += 1
+                    while pitcher_no < len(pitchers2):
                         sim = name_similarity(pitchers2[pitcher_no], s.name)
-                        if sim > max_sim:
+                        if sim >= max_sim:
                             max_sim = sim
                             max_idx = pitcher_no
+                        pitcher_no += 1
                     if not max_idx is None:
                         nm[s.name] = pitchers2[max_idx]
+                        matched = True
                     pitcher_no = 0
 
             # all other subs
@@ -155,9 +178,10 @@ def match_all(lineup, play_list):
     # check if any names are left blank
     blank = [k for k, v in sorted(nm.items(), key=lambda item: item[1]) if v == '']
     pbp_blank = [n for n in pbp_names if not n in nm.values()]
+    subs_blank = [n for n in sub_names if not n in nm.values()]
     for name in blank:
         for n in pbp_blank:
-            if name_similarity(n, name) > .5:
+            if name_similarity(n, name) >= .5:
                 nm[name] = n
 
         if nm[name] == '':
@@ -175,24 +199,30 @@ def match_all(lineup, play_list):
                 elif sub_out[0][1] == 'p':
                     sub_txt = [t for t in game.all_plays(play_list, '') if '/ for ' in t]
                     short = re.search(r'(?<=/ for ).*(?=\.)', sub_txt[0]).group()
-                    if name_similarity(short, sub_out[0][2]) > .5:
+                    if name_similarity(short, sub_out[0][2]) >= .5:
                         nm[sub_out[0][2]] = short
             else:
                 sub_in = [[s.name, s.pos, s.sub] for s in subs if s.name == name]
-                if sub_in[0][1] == 'pr':
-                    sub_type = ' pinch ran'
-                elif sub_in[0][1] == 'ph':
-                    sub_type = ' pinch hit'
-                else:
-                    sub_type = ' to ' + sub_in[0][1]
                 if len(sub_in) > 0:
-                    sub_txt = [t for t in game.all_plays(play_list, '') if sub_type + ' for ' + nm[sub_in[0][2]] in t]
-                    if len(sub_txt) > 0:
-                        nm[sub_in[0][0]] = re.search(r'.*(?=' + sub_type + r' for ' + nm[sub_in[0][2]] + r'\.)', sub_txt[0]).group()
+                    if sub_in[0][1] == 'pr':
+                        sub_type = ' pinch ran'
+                    elif sub_in[0][1] == 'ph':
+                        sub_type = ' pinch hit'
                     else:
-                        sub_txt = [t for t in game.all_plays(play_list, '') if sub_type + ' for ' in t]
-                        if len(sub_txt) == 1:
-                            nm[sub_in[0][0]] = re.search(r'.*(?=' + sub_type + r' for )', sub_txt[0]).group()
+                        sub_type = ' to ' + sub_in[0][1]
+                    if len(sub_in) > 0:
+                        sub_txt = [t for t in game.all_plays(play_list, '') if sub_type + ' for ' + nm[sub_in[0][2]] in t]
+                        if len(sub_txt) > 0:
+                            nm[sub_in[0][0]] = re.search(r'.*(?=' + sub_type + r' for ' + nm[sub_in[0][2]] + r'\.)', sub_txt[0]).group()
+                        else:
+                            sub_txt = [t for t in game.all_plays(play_list, '') if sub_type + ' for ' in t]
+                            if len(sub_txt) == 1:
+                                nm[sub_in[0][0]] = re.search(r'.*(?=' + sub_type + r' for )', sub_txt[0]).group()
+
+        if nm[name] == '':
+            for n in subs_blank:
+                if name_similarity(n, name) >= .7:
+                    nm[name] = n
 
     for player in lineup.lineup:
         if player.name in nm.keys():
@@ -201,6 +231,7 @@ def match_all(lineup, play_list):
             player.pbp_name = player.name
     for player in lineup.subs:
         player.pbp_name = nm[player.name]
+    
     return lineup
     
 def match_helper(box_names, pbp_names):
@@ -226,20 +257,87 @@ def match_helper(box_names, pbp_names):
                     i += 2
     return box_names
 
+def strip_non_alpha(str):
+    return str.replace('.', '').replace(',', ' ').replace("'", '').replace('-', ' ')
+
+def bigram(str):
+    seq = []
+    for i in range(0, len(str)-1):
+        seq.append(str[i:i+2])
+    return(seq)
+
 def name_similarity(part, full):
-    max_score = Levenshtein.ratio(part.title(), full.title())
-    if part.title() in full.title() or part.title().split(' ')[0] in full.title().split(',')[0]:
-        max_score = .5
-    clean = full.replace(',', ' ').replace('-', ' ').replace('.', ' ').replace('  ', ' ')
-    rev = clean.split(' ')
-    rev.reverse()
-    score = Levenshtein.ratio(part.title(), ' '.join(rev))
-    if score > max_score:
-        max_score = score
-    if (len(full) - len(part)) > 5:
-        score = Levenshtein.ratio(part.title(), full.title()[0:len(part)])
-    if score > max_score:
-        max_score = score
-    return max_score
+    p = strip_non_alpha(part).title()
+    f = full
 
+    first = strip_non_alpha(f.split(', ')[-1]).title()
+    last = strip_non_alpha(f.split(', ')[0]).title()
 
+    if first in p and last in p:
+        return 1
+    
+    if last in p and first[0] in p.replace(last, ''):
+        return .9
+
+    if last in p:
+        return .8
+
+    if ' ' in last:
+        if last.split(' ')[0] in p or last.split(' ')[1] in p:
+            return .7
+    
+    if '-' in full:
+        if last.split(' ')[0][0] + '.' + '-' + last.split(' ')[1][0] + '.' in part:
+            return .5
+
+    max_score = 0
+
+    if '. ' in part:
+        if first[0] + '. ' + last[0:len(part)-3] == part:
+            max_score = .5
+    
+    if ' ' in p:
+        bi1 = bigram(last)
+        for pt in p.split(' '):
+            bi2 = bigram(pt)
+            score = compare_bi(bi1, bi2)
+            if score > max_score:
+                max_score = score
+            if one_transpose(pt, last):
+                if .6 > max_score:
+                    max_score = .6
+
+    bi2 = bigram(p)
+    for nm in [last, first]:
+        bi1 = bigram(nm)
+        score = compare_bi(bi1, bi2)
+        if score > max_score:
+            max_score = score
+
+    if one_transpose(p, last):
+        if .6 > max_score:
+            max_score = .6
+
+    print(part + ' VS ' + full + ':' + str(max_score))
+    return(max_score)
+
+def compare_bi(bi1, bi2):  
+    union = len(bi1) + len(bi2)
+
+    if union == 0 or union is None:
+        return 0
+
+    hit_count = 0
+    for x in bi1:
+        for y in bi2:
+            if x == y:
+                hit_count += 1
+                break
+    return((2.0 * hit_count) / union)
+
+def one_transpose(str1, str2):
+    for i in range(2, len(str1)):
+        check = str1[0:i-1] + str1[i] + str1[i-1] + str1[i+1:]
+        if check == str2:
+            return True
+    return False
