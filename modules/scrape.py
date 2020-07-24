1920a5b6ec4af7e7b3f2bb460a1868fd85c84478
Miles Okamoto <48659623+milesok@users.noreply.github.com>
2020-07-24 12:00:17 -0500

    fix bug with cancelled games

--- modules/scrape.py
+++ modules/scrape.py
@@ -119,18 +119,19 @@ def get_scoreboard(date):
     ids = []
     away = []
     home = []
+    links = []
 
     #get elements in td index 3 (away team names and home final scores)
     a_teams = doc.xpath("//div[@id='contentarea']/table/tbody/tr/td[3]")
 
     #
-    for a in range(0, len(a_teams)):
+    for a in range(len(a_teams)):
         if not 'totalcol' in [x for x in a_teams[a].classes]:
             away.append(a_teams[a][0].text if not len(a_teams[a]) < 1 else a_teams[a].text.replace('\n', '').replace('               ', '').replace('            ', ''))
 
     #get elements in td index 2 (away team logos, home team names and blank element below attendance)
     h_teams = doc.xpath("//div[@id='contentarea']/table/tbody/tr/td[2]")
-    for h in range(0, len(h_teams)):
+    for h in range(len(h_teams)):
         if not 'img' in [a.tag for a in h_teams[h]]:
             if not len([a.text for a in h_teams[h]]) > 0:
                 test = h_teams[h].text
@@ -140,7 +141,23 @@ def get_scoreboard(date):
                         home.append(team)
             else:
                 home.append(h_teams[h][0].text)
-    links = doc.xpath("//div[@id='contentarea']/table/tbody/tr/td[1]/a/@href")
+
+    l = doc.xpath("//div[@id='contentarea']/table/tbody/tr/td[1]")
+    na = []
+    for i in range(round(len(l)/3)):
+        e = l[(i)*3+2]
+        if len(e) == 0:
+            na.append(i)
+        else:
+            links.append(e[0].attrib['href'])
+
+    deleted = 0
+    for i in na:
+        del away[i-deleted]
+        del home[i-deleted]
+        deleted += 1
+
+
 
     # Remove rankings and leading spaces
     for i in range(0,len(away)):
@@ -161,7 +178,7 @@ def get_scoreboard(date):
             game.append(0)
         matchups.append(m)
 
-    for j in range(0,len(away)):
+    for j in range(len(away)):
         # Remove records
         if len(re.search(r'([0-9]{1,2}-[0-9]{1,2})', home[j]).group()):
             home[j] = home[j].replace(' (' + home[j].split(' (')[-1], '')
